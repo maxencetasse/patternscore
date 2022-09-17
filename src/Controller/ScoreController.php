5338aca2054f62dd2dbe5d428c848dc7c23aaf34
Logulasse <maxence.tasse@gmail.com>
2022-09-17 16:54:23 +0200

    Mise en place des premières interfaces. Ajout de la méthode result dans le controller pour obtenir le résultat.

--- src/Controller/ScoreController.php
+++ src/Controller/ScoreController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,6 +12,16 @@ class ScoreController extends AbstractController
     #[Route('/score', name: 'app_score')]
     public function index(): Response
     {
+        return $this->render('score/index.html.twig', [
+            'controller_name' => 'ScoreController',
+        ]);
+    }
+
+    #[Route('/score/result', name: 'app_score_result')]
+    public function result(Request $request): Response
+    {
+        //dump($request->get);
+
         return $this->render('score/index.html.twig', [
             'controller_name' => 'ScoreController',
         ]);
